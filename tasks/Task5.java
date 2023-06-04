4247f28549e5199beb2cd33fe3e813e5abc44962
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-06-04 18:48:06 +0300

    add method that shuffles two streams

--- tasks/Task5.java
+++ tasks/Task5.java
@@ -1,4 +1,20 @@
 package tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task5 {
+    public static <T> Stream<T> zip(Stream<T> streamA, Stream<T> streamB){
+        List<T> streamAItems = streamA.collect(Collectors.toList());
+        List<T> streamBItems = streamB.collect(Collectors.toList());
+        int minSize = Math.min(streamAItems.size(), streamBItems.size());
+        List<T> resultList = new ArrayList<>();
+        for (int i = 0; i < minSize; i++) {
+            resultList.add(streamAItems.get(i));
+            resultList.add(streamBItems.get(i));
+        }
+        return resultList.stream();
+    }
 }
